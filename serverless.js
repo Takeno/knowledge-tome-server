2fe60093f48c2d3a0ed76f4b98a3c4aec212a517
Matteo Manchi <matteo.manchi@gmail.com>
2018-08-29 15:09:57 +0200

    CORS for /articles

--- serverless.js
+++ serverless.js
@@ -23,6 +23,10 @@ module.exports.articles = async event => {
 
     const response = {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
         body: JSON.stringify(posts),
     };
 
